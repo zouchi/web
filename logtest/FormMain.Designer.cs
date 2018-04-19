8bee1954bb23f1d43f347fc7cfa4c557cf659828
zouchi <824229769@qq.com>
2018-04-19 16:51:21 +0800

    添加CEF网页框架

--- logtest/FormMain.Designer.cs
+++ logtest/FormMain.Designer.cs
@@ -33,15 +33,15 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(380, 152);
+            this.button1.Location = new System.Drawing.Point(689, 418);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 103);
+            this.button1.Size = new System.Drawing.Size(110, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // Form1
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
--- logtest/FormMain.cs
+++ logtest/FormMain.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using log4net;
+using CefSharp;
+using CefSharp.WinForms;
 namespace logtest
 {
     public partial class FormMain : Form
@@ -14,13 +16,28 @@ namespace logtest
         public FormMain()
         {
             InitializeComponent();
+            InitBrowser();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
 
             Applog.LogDebug("log", "测试Log4Net日志是否写入");
             Applog.LogError("Error", "测试Log4Net日志是否写入");
+            
+            MessageBox.Show("打开成功", "按钮测试");
         }
+
+
+        public ChromiumWebBrowser browser;
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("www.baidu.com");
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+        }
+
+
     }
 }
